e7c3727708766354fded3a9dc9479eae67c472b4
CLB <CLB.Projecto@gmail.com>
2020-04-12 16:33:04 +0100

    First Commit For Repo

---
+++ src/test/java/mediastore/MediaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mediastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
